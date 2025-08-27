29d26fbd1dcd1cec244f19113651d2b33e910a34
prasetyasurya-ui <prasetya.surya@ui.ac.id>
2025-08-27 13:27:50 +0700

    fixed typo

--- football_news/settings.py
+++ football_news/settings.py
@@ -29,7 +29,7 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == True
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "prasetya.surya-footballnews.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "prasetya-surya-footballnews.pbp.cs.ui.ac.id"]
 
 
 # Application definition
